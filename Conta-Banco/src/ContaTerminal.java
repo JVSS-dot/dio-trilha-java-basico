5d63876bc35bf155d43aecf0ecdb397d3921084b
João Victor <jvss.trab@gmail.com>
2023-10-21 20:51:29 -0300

    apenas um teste

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner Scanner = new Scanner(System.in);
+        String NumeroConta;
+        String Agencia;
+        String NomeCliente;
+        double Saldo;
+        System.out.println("Por favor, digite o número da Agência!");
+        Agencia = Scanner.nextLine();
+
+        System.out.println("Por favor, digite o Número!");
+        NumeroConta = Scanner.nextLine();
+
+        System.out.println("Por favor, digite seu nome e sobrenome.");
+        NomeCliente = Scanner.nextLine();
+
+        System.out.println("Por favor, insira seu saldo.");
+        Saldo = Scanner.nextDouble();
+
+        
     }
+
 }
